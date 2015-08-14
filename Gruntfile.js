c77d5c01fdaa8b4154b2d2a062052c8d6248a743
Mathieu ROBERT <mathieu.robert@yocto.re>
2015-08-14 23:32:34 +0400

    Intégration de doctrap + modification de la génération de la doc + ajout d'un début de style pour yocto.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
       options : {
         name        : 'Yoctodoc plugin',
       },
-      all     : [ 'tasks/yoctodoc.js' ]
+      all     : [ 'test/index-test.js' ]
     }
   });
 
--- tasks/yoctodoc.js
+++ tasks/yoctodoc.js
@@ -2,9 +2,12 @@
 
 var _         = require('lodash');
 var hooker    = require('hooker');
+var beautify  = require('html');
+var glob      = require('glob');
 
 /**
  * Default export for grunt yocto-doc-plugin
+ *
  * @module yoctodoc/grunt
  */
 module.exports = function (grunt) {
@@ -12,7 +15,11 @@ module.exports = function (grunt) {
    * @property configPath {String} Default configPath
    * @private
    */
-  var configPath   = [ __dirname, 'yoctodoc.json' ].join('/');
+  var configPath    = [ __dirname, 'yoctodoc.json' ].join('/');
+  // default css to include
+  var cssToInclude  = '<link type="text/css" rel="stylesheet" href="styles/yoctostyle.css"></head>';
+  // default choosen themes
+  var choosenTheme;
 
   // Default yocto config for jshint and jscs code style
   var defaultOptions = {
@@ -21,7 +28,14 @@ module.exports = function (grunt) {
         options : {
           destination : [ process.cwd(), 'documentation' ].join('/'),
           configure   : configPath,
-          template    : [ process.cwd(), 'node_modules', 'jaguarjs-jsdoc' ].join('/'),
+          template    : [
+            process.cwd(),
+            'node_modules',
+            'grunt-jsdoc',
+            'node_modules',
+            'ink-docstrap',
+            'template'
+          ].join('/'),
           readme      : [ process.cwd(), 'README.md' ].join('/')
         },
         src     : []
@@ -78,6 +92,44 @@ module.exports = function (grunt) {
 
           // Need to log end message with custom param
           grunt.log[cMsg.level](cMsg.msg);
+
+          // Add yocto style in fo file
+          grunt.log.ok('Move & Link yoctostyle.css into index.html');
+
+          // needed property
+          var htmlIndexPath = [ process.cwd(), 'documentation', 'index.html' ].join('/');
+
+          // get current html
+          var html          = grunt.file.read(htmlIndexPath);
+
+          // replace html content to add our style
+          html = html.replace(/<\/head>/gm, cssToInclude);
+
+          // copy custom style on doc style directory
+          grunt.file.copy(
+            [ __dirname, 'yoctostyle.css' ].join('/'),
+            [ grunt.config.data.jsdoc.dist.options.destination,
+            'styles/yoctostyle.css' ].join('/')
+          );
+
+          // saving new template content
+          grunt.file.write(htmlIndexPath, beautify.prettyPrint(html,  { indexSize : 2 }));
+
+          // removing no needed file
+          var unused = glob.sync([
+            grunt.config.data.jsdoc.dist.options.destination,
+            'styles',
+            'site*.css'
+          ].join('/'));
+
+          // read & test all file
+          _.each(unused, function (u) {
+            // test if is not the current theme
+            if (_.isNull(u.match([ 'site.', choosenTheme, '.css' ].join('')))) {
+              grunt.file.delete(u);
+            }
+          });
+          grunt.log.ok('Removing unused files finished.');
         }
       }
     });
@@ -133,13 +185,16 @@ module.exports = function (grunt) {
       // assign
       appName = options.name;
       // inform
-      grunt.log.ok([ 'New name added :', appName ].join(' '));
+      grunt.log.ok([ 'New name added : [ ', appName, ' ] - Processing update.' ].join(''));
     }
 
     // change config of file for app name
     try {
       var configjsdoc = JSON.parse(grunt.file.read(configPath));
+      // assign the application name
       configjsdoc.templates.applicationName = appName;
+      choosenTheme                          = configjsdoc.templates.theme;
+      // change config file
       grunt.file.write(configPath, JSON.stringify(configjsdoc, null, 2));
 
     } catch (e) {
@@ -152,7 +207,7 @@ module.exports = function (grunt) {
       grunt.log.ok([
         this.data.length,
         [ 'file', (this.data.length > 1 ? 's' : '') ].join(''),
-        'found. Adding on hinter list.'
+        'found. Adding on source list.'
       ].join(' '));
 
       // Assign default options to config file
